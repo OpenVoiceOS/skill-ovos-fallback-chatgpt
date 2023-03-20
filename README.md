bc081e09ef3afb7bd19dac429df8dd617857be76
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-03-20 03:02:32 +0000

    feat/solver_plugin (#5)
    migrate to https://github.com/OpenVoiceOS/ovos-solver-plugin-openai-persona

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-# ChatGPT skill
+# ChatGPT Persona Fallback Skill
 
-When in doubt, ask chatgpt
+When in doubt, ask chatgpt, powered by [OVOS Persona](https://github.com/OpenVoiceOS/ovos-solver-plugin-openai-persona)
 
 You need to configure an api_key, get it at https://beta.openai.com/account/api-keys
 
 
 ## About 
 
-ChatGPT skill
-
 Capabilities:
 - Remembers what user said earlier in the conversation
-- Allows user to provide follow-up corrections
 - Trained to decline inappropriate requests
 
 Limitations:
@@ -26,22 +23,9 @@ Under skill settings you can tweak some parameters for chatGPT
 
 
 - `key` - your api_key to access OpenAI
-- `initial_prompt` - can be used to create a "persona", give a personality to chatGPT
-- `memory` - remember previous conversations (since loading), default `true`
-- TODO - all request params will be exposed in the future, default values are
-      `engine="davinci", temperature=0.85, 
-      top_p=1, frequency_penalty=0,
-      presence_penalty=0.7, best_of=2, 
-      max_tokens=100`
+- `persona` - can be used to create a "persona", give a personality to chatGPT
 
-
-The default initial prompt is
-
-```
-The assistant is helpful, creative, clever, and very friendly.
-```
-
-Check out [awesome-chatgpt-prompts](https://github.com/f/awesome-chatgpt-prompts) for ideas
+The default persona is `helpful, creative, clever, and very friendly.`
 
 
 ## Examples 
--- __init__.py
+++ __init__.py
@@ -1,16 +1,13 @@
-import openai as ai
 from ovos_workshop.skills.fallback import FallbackSkill
 from ovos_utils.process_utils import RuntimeRequirements
 from ovos_utils import classproperty
+from ovos_solver_openai_persona import OpenAIPersonaSolver
 
 
 class ChatGPTSkill(FallbackSkill):
 
     def __init__(self):
         super().__init__("ChatGPT")
-        self._chat = None
-        self.max_utts = 15  # memory size TODO from skill settings
-        self.qa_pairs = []  # tuple of q+a
         self.current_q = None
         self.current_a = None
 
@@ -27,12 +24,15 @@ class ChatGPTSkill(FallbackSkill):
                                    no_gui_fallback=True)
 
     def initialize(self):
+        self._chat = OpenAIPersonaSolver(config=self.settings)
         self.add_event("speak", self.handle_speak)
         self.add_event("recognizer_loop:utterance", self.handle_utterance)
         self.register_fallback(self.ask_chatgpt, 85)
 
     def handle_utterance(self, message):
         utt = message.data.get("utterances")[0]
+        if self.current_q and self.current_a:
+            self._chat.qa_pairs.append((self.current_q, utt))
         self.current_q = utt
         self.current_a = None
         # TODO: imperfect, subject to race conditions between bus messages
@@ -44,77 +44,16 @@ class ChatGPTSkill(FallbackSkill):
             # TODO - use session_id/ident to track all matches
             # append to previous question if multi-speak answer
             return
-        if utt and self.memory:
-            self.qa_pairs.append((self.current_q, utt))
-        self.current_q = None
-        self.current_a = None
-
-    @property
-    def memory(self):
-        return self.settings.get("memory", True)
-
-    @property
-    def engine(self):
-        return self.settings.get("engine", "ada")
-
-    @property
-    def initial_prompt(self):
-        start_chat_log = """The assistant is helpful, creative, clever, and very friendly."""
-        s = self.settings.get("initial_prompt", start_chat_log)
-        return f"The following is a conversation with an AI assistant. {s}"
-
-    @property
-    def chatgpt(self):
-        # this is a property to allow lazy init
-        # the key may be set after skill is loaded
-        key = self.settings.get("key")
-        if not key:
-            raise ValueError("OpenAI api key not set in skill settings.json")
-        if not self._chat:
-            ai.api_key = key
-            self._chat = ai.Completion()
-        return self._chat
-
-    @property
-    def chat_history(self):
-        # TODO - intro question from skill settings
-        intro_q = ("Hello, who are you?", "I am an AI created by OpenAI. How can I help you today?")
-        if len(self.qa_pairs) > self.max_utts:
-            qa = [intro_q] + self.qa_pairs[-1*self.max_utts:]
-        else:
-            qa = [intro_q] + self.qa_pairs
-        chat = self.initial_prompt.strip() + "\n\n"
-        if qa:
-            qa = "\n".join([f"Human: {q}\nAI: {a}" for q, a in qa])
-            if chat.endswith("\nHuman: "):
-                chat = chat[-1*len("\nHuman: "):]
-            if chat.endswith("\nAI: "):
-                chat += f"Please rephrase the question\n"
-            chat += qa
-        return chat
-
-    def get_prompt(self, utt):
-        self.current_q = None
-        self.current_a = None
-        prompt = self.chat_history
-        if not prompt.endswith("\nHuman: "):
-            prompt += f"\nHuman: {utt}?\nAI: "
-        else:
-            prompt += f"{utt}?\nAI: "
-        return prompt
+        if self.current_a:
+            self.current_a += ". " + utt
 
     def ask_chatgpt(self, message):
+        self.current_a = None
         utterance = message.data['utterance']
-        prompt = self.get_prompt(utterance)
-        # TODO - params from skill settings
-        response = self.chatgpt.create(prompt=prompt, engine=self.engine, temperature=0.85,
-                                       top_p=1, frequency_penalty=0,
-                                       presence_penalty=0.7, best_of=2, max_tokens=200, stop="\nHuman: ")
-        answer = response.choices[0].text.split("Human: ")[0].split("AI: ")[0].strip()
-        if not answer or not answer.strip("?") or not answer.strip("_"):
+        answer = self._chat.get_spoken_answer(utterance)
+        if not answer:
             return False
-        if self.memory:
-            self.qa_pairs.append((utterance, answer))
+        self.current_q = None
         self.speak(answer)
         return True
 
@@ -122,27 +61,3 @@ class ChatGPTSkill(FallbackSkill):
 def create_skill():
     return ChatGPTSkill()
 
-
-if __name__ == "__main__":
-    from ovos_utils.messagebus import FakeBus, Message
-
-    s = ChatGPTSkill()
-    s._startup(bus=FakeBus())
-    msg = Message("intent_failure", {"utterance": "Explain quantum computing in simple terms"})
-    s.ask_chatgpt(msg)
-    print(s.qa_pairs[-1])
-    msg = Message("intent_failure", {"utterance": "Got any creative ideas for a 10 year old’s birthday?"})
-    s.ask_chatgpt(msg)
-    print(s.qa_pairs[-1])
-    msg = Message("intent_failure", {"utterance": "Do you think aliens exist?"})
-    s.ask_chatgpt(msg)
-    print(s.qa_pairs[-1])
-    msg = Message("intent_failure", {"utterance": "When will the world end?"})
-    s.ask_chatgpt(msg)
-    print(s.qa_pairs[-1])
-    print("##################################3")
-    print(s.chat_history)
-    # funny failure cases:
-    #    ????
-    #    Are you seriously asking that?
-    #    I hardly understand quantum physics myself, but I found an article that you might find useful.
