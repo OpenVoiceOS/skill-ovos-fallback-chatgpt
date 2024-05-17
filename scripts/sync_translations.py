cca8bc7509845a041b3417c27effc12c9e4efb84
miro <jarbasai@mailfence.com>
2024-05-17 01:05:09 +0100

    update translation automations
    handle more corner cases, missing subdirectories, deduplicate files

--- scripts/sync_translations.py
+++ scripts/sync_translations.py
@@ -20,11 +20,14 @@ for lang in os.listdir(tx):
         with open(intents) as f:
             data = json.load(f)
         for fid, samples in data.items():
-
             if samples:
-                os.makedirs(f"{locale}/{lang.lower()}", exist_ok=True)
-                samples = [s.strip() for s in samples 
-                           if s and s.strip() != "[UNUSED]"]  # s may be None
+                samples = list(set([s.strip() for s in samples
+                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                if fid.startswith("/"):
+                    p = f"{locale}/{lang.lower()}{fid}"
+                else:
+                    p = f"{locale}/{lang.lower()}/{fid}"
+                os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
                     f.write("\n".join(sorted(samples)))
 
@@ -32,11 +35,14 @@ for lang in os.listdir(tx):
         with open(dialogs) as f:
             data = json.load(f)
         for fid, samples in data.items():
-
             if samples:
-                os.makedirs(f"{locale}/{lang.lower()}", exist_ok=True)
-                samples = [s.strip() for s in samples 
-                           if s and s.strip() != "[UNUSED]"]  # s may be None
+                samples = list(set([s.strip() for s in samples
+                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                if fid.startswith("/"):
+                    p = f"{locale}/{lang.lower()}{fid}"
+                else:
+                    p = f"{locale}/{lang.lower()}/{fid}"
+                os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
                     f.write("\n".join(sorted(samples)))
 
@@ -44,11 +50,14 @@ for lang in os.listdir(tx):
         with open(vocs) as f:
             data = json.load(f)
         for fid, samples in data.items():
-
             if samples:
-                os.makedirs(f"{locale}/{lang.lower()}", exist_ok=True)
-                samples = [s.strip() for s in samples 
-                           if s and s.strip() != "[UNUSED]"]  # s may be None
+                samples = list(set([s.strip() for s in samples
+                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                if fid.startswith("/"):
+                    p = f"{locale}/{lang.lower()}{fid}"
+                else:
+                    p = f"{locale}/{lang.lower()}/{fid}"
+                os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
                     f.write("\n".join(sorted(samples)))
 
@@ -56,11 +65,14 @@ for lang in os.listdir(tx):
         with open(regexes) as f:
             data = json.load(f)
         for fid, samples in data.items():
-
             if samples:
-                os.makedirs(f"{locale}/{lang.lower()}", exist_ok=True)
-                samples = [s.strip() for s in samples 
-                           if s and s.strip() != "[UNUSED]"]  # s may be None
+                samples = list(set([s.strip() for s in samples
+                           if s and s.strip() != "[UNUSED]"]))  # s may be None
+                if fid.startswith("/"):
+                    p = f"{locale}/{lang.lower()}{fid}"
+                else:
+                    p = f"{locale}/{lang.lower()}/{fid}"
+                os.makedirs(os.path.dirname(p), exist_ok=True)
                 with open(f"{locale}/{lang.lower()}/{fid}", "w") as f:
                     f.write("\n".join(sorted(samples)))
 
